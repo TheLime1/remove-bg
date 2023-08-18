46425fbb1bdec26cb4464f056c0916e333ec986d
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-08-18 02:06:57 +0100

    +crop

--- bg_removed/upscale.py
+++ bg_removed/upscale.py
@@ -1,13 +1,26 @@
 import os
 from PIL import Image
 
+def crop_image(image_path, output_path):
+    with Image.open(image_path) as im:
+        width, height = im.size
+        left = width * 0.1
+        top = height * 0.1
+        right = width * 0.9
+        bottom = height * 0.9
+        im = im.crop((left, top, right, bottom))
+        bbox = im.getbbox()
+        if bbox:
+            im = im.crop(bbox)
+        im.save(output_path)
+
 # Create a subdirectory named "originals" if it does not already exist
 os.makedirs("originals", exist_ok=True)
 
-# Get a list of all image files in the current working directory that do not have the suffix "_upscaled" in their filenames
-image_files = [f for f in os.listdir() if f.endswith((".jpg", ".jpeg", ".png")) and not f.endswith("_upscaled.png")]
+# Get a list of all image files in the current working directory that do not have the suffix "_upscaled_cropped" in their filenames
+image_files = [f for f in os.listdir() if f.endswith((".jpg", ".jpeg", ".png")) and not f.endswith("_upscaled_cropped.png")]
 
-# Iterate over the image files, upscale them, and move the originals to the "originals" subdirectory
+# Iterate over the image files, upscale them, crop them, and move the originals to the "originals" subdirectory
 for image_file in image_files:
     # Open the image file
     img = Image.open(image_file)
@@ -23,7 +36,12 @@ for image_file in image_files:
     upscaled_img = img.resize((new_width, new_height), resample=Image.LANCZOS)
     
     # Save the upscaled image with the suffix "_upscaled" added to its filename and with the ".png" extension
-    upscaled_img.save(f"{os.path.splitext(image_file)[0]}_upscaled.png")
+    upscaled_image_path = f"{os.path.splitext(image_file)[0]}_upscaled.png"
+    upscaled_img.save(upscaled_image_path)
+    
+    # Crop the upscaled image using the crop_image function and save it with an additional suffix "_cropped" added to its filename
+    cropped_image_path = f"{os.path.splitext(image_file)[0]}_upscaled_cropped.png"
+    crop_image(upscaled_image_path, cropped_image_path)
     
     # Move the original image file to the "originals" subdirectory
     os.rename(image_file, os.path.join("originals", image_file))
