b86f05a79f6d585ea8e7dc79f7ea22ea2816070f
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-08-18 01:30:23 +0100

    upscale

--- bg_removed/upscale.py
+++ bg_removed/upscale.py
@@ -1,23 +1,29 @@
 import os
-from super_image import EdsrModel, ImageLoader
 from PIL import Image
 
-# Create an EdsrModel object
-model = EdsrModel.from_pretrained('eugenesiow/edsr-base', scale=2)
+# Create a subdirectory named "originals" if it does not already exist
+os.makedirs("originals", exist_ok=True)
 
-# Get a list of all image files in the current working directory
-image_files = [f for f in os.listdir() if f.endswith((".jpg", ".jpeg", ".png"))]
+# Get a list of all image files in the current working directory that do not have the suffix "_upscaled" in their filenames
+image_files = [f for f in os.listdir() if f.endswith((".jpg", ".jpeg", ".png")) and not f.endswith("_upscaled.png")]
 
-# Iterate over the image files and upscale them
+# Iterate over the image files, upscale them, and move the originals to the "originals" subdirectory
 for image_file in image_files:
-    # Load the image
+    # Open the image file
     img = Image.open(image_file)
     
-    # Prepare the inputs
-    inputs = ImageLoader.load_image(img)
+    # Get the size of the image
+    width, height = img.size
     
-    # Upscale the image
-    preds = model(inputs)
+    # Calculate the new size of the image
+    new_width = width * 2
+    new_height = height * 2
     
-    # Save the upscaled image
-    ImageLoader.save_image(preds, f"upscaled_{image_file}")
+    # Upscale the image while preserving its transparency
+    upscaled_img = img.resize((new_width, new_height), resample=Image.LANCZOS)
+    
+    # Save the upscaled image with the suffix "_upscaled" added to its filename and with the ".png" extension
+    upscaled_img.save(f"{os.path.splitext(image_file)[0]}_upscaled.png")
+    
+    # Move the original image file to the "originals" subdirectory
+    os.rename(image_file, os.path.join("originals", image_file))
